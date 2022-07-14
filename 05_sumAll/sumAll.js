55af0ea9981c832588741118445b8ab2ff7761cb
MCodes96 <mikayil_gacek@outlook.com>
2022-07-14 15:37:10 +0200

    Complete function to pass all tests

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,19 @@
-const sumAll = function() {
+const sumAll = function (a, b) {
+  let sum = 0;
+  let smallInt = a;
+  let bigInt = b;
 
+  if (!Number.isFinite(a) || !Number.isFinite(b) || a < 0 || b < 0) {
+    return 'ERROR';
+  } else if (b < a) {
+    smallInt = b;
+    bigInt = a;
+  }
+
+  for (let i = smallInt; i <= bigInt; i++) {
+    sum += i;
+  }
+  return sum;
 };
 
 // Do not edit below this line
