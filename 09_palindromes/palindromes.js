f94099ef93fa1f94cdb676c731c1a7700a7517a9
Mikayil Gacek <mikayil_gacek@outlook.com>
2022-07-15 13:45:50 +0200

    Complete function to pass all tests.

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,10 @@
-const palindromes = function () {
-
+const palindromes = function (str) {
+  let cleanString = str.toLowerCase().replace(/[^a-z0-9]/g, '');
+  if (cleanString === cleanString.split('').reverse().join('')) {
+    return true;
+  } else {
+    return false;
+  }
 };
 
 // Do not edit below this line
